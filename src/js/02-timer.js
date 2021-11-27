3d6ae8f715dbb3210edc97864ad8336409b5d68a
PatrykJarzynka <patryk.jarzynka1234@gmail.com>
2021-11-27 13:49:14 +0100

    Ukończono zadanie 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -7,7 +7,6 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
     let date = selectedDates[0];
     if (date.getTime() < actuallTime) {
       window.alert('Please choose a date in the future');
@@ -19,19 +18,14 @@ const options = {
 };
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
@@ -39,6 +33,10 @@ function convertMs(ms) {
 
 const data = document.querySelector('#datetime-picker');
 const btn = document.querySelector('button[data-start]');
+const days = document.querySelector('span[data-days]');
+const hours = document.querySelector('span[data-hours]');
+const minutes = document.querySelector('span[data-minutes]');
+const seconds = document.querySelector('span[data-seconds]');
 const actuallTime = options.defaultDate.getTime();
 let newTime = 0;
 let timerId = null;
@@ -47,7 +45,11 @@ let cos = flatpickr(data, options);
 btn.setAttribute('disabled', true);
 btn.addEventListener('click', () => {
   timerId = setInterval(() => {
-    console.log(convertMs(newTime - actuallTime));
+    let time = convertMs(newTime - actuallTime);
+    days.textContent = time.days;
+    hours.textContent = time.hours;
+    minutes.textContent = time.minutes;
+    seconds.textContent = time.seconds;
     newTime -= 1000;
   }, 1000);
 });
