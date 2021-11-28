3c462e17bf9cc66cb543c1be17321705638599a2
PatrykJarzynka <patryk.jarzynka1234@gmail.com>
2021-11-28 14:52:22 +0100

    Zaczęto tworzyć zadanie 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,30 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        
+      },delay)
+    })
   } else {
     // Reject
   }
 }
+
+const delay = document.querySelector("input[name=delay]");
+const step = document.querySelector('input[name=step]');
+const amount = document.querySelector('input[name=amount');
+const btn = document.querySelector("button[type=submit]");
+
+btn.addEventListener("click", () => {
+  for (let i = 0; i < amount.value; i++){
+    createPromise(i, delay.value)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+})
+
